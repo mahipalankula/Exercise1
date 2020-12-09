fd48f231e5a46621c9d53fab1593ecba4c8c9d58
mahipalankula <75672166+mahipalankula@users.noreply.github.com>
2020-12-09 09:19:33 +0530

    Update TestRunner.java

--- Exercise1/src/main/java/com/qa/Runner/TestRunner.java
+++ Exercise1/src/main/java/com/qa/Runner/TestRunner.java
@@ -9,7 +9,7 @@ import cucumber.api.junit.Cucumber;
 		
 		features = "src\\main\\java\\com\\qa\\Features\\problem.feature", // Path of the feature file
 		glue = {"com.qa.StepDefinitions"}, // Path of the Step Definition file
-		format = {"pretty", "html:test-output"}, // To display in a proper format
+		format = {"pretty", "html:test-output"}, // To display output in a proper format
 		monochrome = true, //To display the console output in readable format
 		strict = false, //it will check if any step is not defined in the step definition file
 		dryRun = false //to check the mapping between Feature file and Step Def file
